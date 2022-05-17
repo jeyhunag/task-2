a2e1ef20c75c129cb0197beb02f78883194f02af
*jeyhun <*jeyhunag@code.edu.az*>
2022-05-17 12:41:58 +0300

    commit

---
+++ task 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace task_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int val1 = 1;
+            int val2 = 100;
+            int sum = 0;
+            for (int i = val1; i <= val2; i++)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
